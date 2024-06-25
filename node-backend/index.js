4a0d1d86a724d382ebb2cf2bc8c24b91e75e212f
sweetArizCider <arizah.applecider+github@gmail.com>
2024-06-24 20:41:20 -0600

    despues de clase de garay

--- node-backend/index.js
+++ node-backend/index.js
@@ -30,8 +30,8 @@ app.use(session({
 
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'inicio',
+    user: 'applecider',
+    password: 'start',
     database: 'nodelogin'
 });
 
@@ -161,7 +161,7 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });

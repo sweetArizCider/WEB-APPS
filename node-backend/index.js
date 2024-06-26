4767451f1246829a9f032c3ef6638bc4856b0564
sweetArizCider <arizah.applecider+github@gmail.com>
2024-06-25 18:53:19 -0600

    hola

--- node-backend/index.js
+++ node-backend/index.js
@@ -160,7 +160,7 @@ app.get('/', (req, res) => {
     res.redirect('/login');
 });
 
-const PORT = process.env.PORT || 3000; // Default to port 3000 if PORT environment variable is not set
+const PORT = process.env.PORT || 80; // Default to port 3000 if PORT environment variable is not set
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
